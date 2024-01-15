f649701b5402f6d25d946734cd021c5fc1c2f363
fire-mango <2197423361@qq.com>
2024-01-15 22:23:31 +0800

    add .gitigonroe

---
+++ module3/src/com/mango/helloworld/Demo01HelloWorld.java
@@ -0,0 +1,13 @@
+package com.mango.helloworld;
+
+public class Demo01HelloWorld {
+    public static void main(String[] args) {
+        // 输出 Hello, World!
+        System.out.println("Hello, World!");
+        int i = 10;
+        System.out.println(i);
+        System.out.println("i = " + i);
+        // alt + enter
+        int x = (int) 2.5;
+    }
+}
