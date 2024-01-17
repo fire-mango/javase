4e2a79064b5f8cd10a1da52ea8cbd9864cbd7e04
fire-mango <2197423361@qq.com>
2024-01-17 22:14:53 +0800

    add Demo02Arithmetic

---
+++ module3/src/com/mango/arithmetic/Demo01Arithmetic.java
@@ -0,0 +1,18 @@
+package com.mango.arithmetic;
+
+public class Demo01Arithmetic {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 3;
+        int add = i + j;
+        System.out.println("add=" + add);
+        int sub = i - j;
+        System.out.println("sub=" + sub);
+        int mul = i * j;
+        System.out.println("mul=" + mul);
+        int div = i / j;
+        System.out.println("div=" + div);
+        int mod = i % j;
+        System.out.println("mod=" + mod);
+    }
+}
---
+++ module3/src/com/mango/arithmetic/Demo02Arithmetic.java
@@ -0,0 +1,11 @@
+package com.mango.arithmetic;
+
+public class Demo02Arithmetic {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 3;
+        System.out.println("i + j = " + (i + j + ""));
+        System.out.println("i + j = " + (i + j + "" + 1));
+        System.out.println("i + j = " + (i + "" + j));
+    }
+}
