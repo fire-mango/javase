bc1a1e63b9c801cbd6965bb972d9fdf9290c6fc6
fire-mango <2197423361@qq.com>
2025-02-19 16:17:16 +0800

    add extends study

---
+++ module10/src/com/mango/e_extends/NewPhone.java
@@ -0,0 +1,9 @@
+package com.mango.e_extends;
+
+public class NewPhone extends OldPhone{
+    public void show(){
+        System.out.println("显示手机号");
+        System.out.println("展示xxx");
+        System.out.println("展示xxx");
+    }
+}
