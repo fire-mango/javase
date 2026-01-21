6810bcd01643cfa037b171eb0f3077785ac9ec0b
fire-mango <2197423361@qq.com>
2026-01-21 17:12:58 +0800

    add string study

---
+++ module14/src/com/mango/a_string/Demo01String.java
@@ -0,0 +1,9 @@
+package com.mango.a_string;
+
+public class Demo01String {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "hello";
+        System.out.println(s1 == s2);
+    }
+}
---
+++ module14/src/com/mango/a_string/Demo03String.java
@@ -0,0 +1,12 @@
+package com.mango.a_string;
+
+public class Demo03String {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = new String("hello");
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s2 == s3);
+    }
+}
